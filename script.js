d4e55f454034f08483354fb43ceeaf5fe7bfadd1
ira-lapchuk <114941166+ira-lapchuk@users.noreply.github.com>
2022-11-04 10:10:52 +0200

    add color button

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
+
+function showRed() {
+    const backgroundRedEl = document.getElementById("bkg-red");
+    let shapeEl = document.getElementById("shp");
+
+    shapeEl.style.backgroundColor = "red";
+}
+
+function showBlue() {
+    const backgroundBlueEl = document.getElementById("bkg-blue");
+    let shapeEl = document.getElementById("shp");
+
+    shapeEl.style.backgroundColor = "blue";
+}
+    
+function showGreen() {
+    const backgroundGreenEl = document.getElementById("bkg-green");
+    let shapeEl = document.getElementById("shp");
+
+    shapeEl.style.backgroundColor = "green";
+}
+
+function showBorderRed() {
+    const borderRedEl = document.getElementById("brd-red");
+    let shapeEl = document.getElementById("shp");
+
+    shapeEl.style.borderColor = "red";
+}
+
+function showBorderBlue() {
+    const borderBlueEl = document.getElementById("brd-blue");
+    let shapeEl = document.getElementById("shp");
+
+    shapeEl.style.borderColor = "blue";
+ }
+    
+function showBorderGreen() {
+    const borderGreenEl = document.getElementById("brd-green");
+    let shapeEl = document.getElementById("shp");
+
+    shapeEl.style.borderColor = "green";
+}  
+
 function showResult(){
-    const backgroundColorEl = document.getElementById("bgd-color");
-    const borderColorEl = document.getElementById("br-color");
     const borderRadiusEl = document.getElementById("br-rds");
     const borderWidthEl = document.getElementById("br-w");
 
     let shapeEl = document.getElementById("shp");
 
     
-    shapeEl.style.backgroundColor = backgroundColorEl.value;
-    shapeEl.style.borderColor = borderColorEl.value;
     shapeEl.style.borderRadius = borderRadiusEl.value;
-    shapeEl.style.borderWidth = borderWidthEl.value;
+    shapeEl.style.borderWidth = borderWidthEl.value;  
 }
